60271009eac15706be50fdb8f7f765797396b633
gඞbi <gyabii@proton.me>
2026-03-27 19:03:27 +0200

    bootinfo.c: fix unused result

--- techpack/bootinfo/bootinfo.c
+++ techpack/bootinfo/bootinfo.c
@@ -82,7 +82,8 @@ static int get_powerup_reason(char *buf)
 		pr_err("get_powerup_reason: pu_reason is null, powerup_reason:%s\n",powerup_reason);
 		return -EINVAL;
 	}
-	kstrtou32(buf, 16, &pu_reason);
+	if (kstrtou32(buf, 16, &pu_reason)) {
+    pr_err("bootinfo: failed to parse pu_reason\n");
 	return 0;
 }
 
@@ -92,7 +93,8 @@ static int get_powerdown_reason(char *buf)
 		pr_err("get_powerdown_reason: pd_reason is null, powerdown_reason:%s\n", powerdown_reason);
 		return -EINVAL;
 	}
-	kstrtou32(buf, 16, &pd_reason);
+	if (kstrtou32(buf, 16, &pd_reason)) {
+    pr_err("bootinfo: failed to parse pd_reason\n");
 	return 0;
 }
 
