7c5f156f87229dccbf431dcd8a64efb741eaeff7
gඞbi <gyabii@proton.me>
2026-03-28 21:54:17 +0200

    Update thermal_core.c

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1979,7 +1979,7 @@ static ssize_t
 thermal_board_sensor_temp_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
-	snprintf(board_sensor_temp, PAGE_SIZE, buf);
+	snprintf(board_sensor_temp, sizeof(board_sensor_temp), "%s", buf);
 
 	return len;
 }
@@ -1998,7 +1998,7 @@ static ssize_t
 thermal_board_sensor_second_temp_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
-	snprintf(board_sensor_second_temp, PAGE_SIZE, buf);
+	snprintf(board_sensor_second_temp, sizeof(board_sensor_second_temp), "%s", buf);
 
 	return len;
 }
