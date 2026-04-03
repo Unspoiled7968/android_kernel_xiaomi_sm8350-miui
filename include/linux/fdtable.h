14d65323179a74c75ad4bb6bec126415a89e628a
gඞbi <gyabii@proton.me>
2026-04-03 14:39:23 +0300

    fix

--- include/linux/fdtable.h
+++ include/linux/fdtable.h
@@ -112,7 +112,7 @@ int unshare_files(struct files_struct **);
 struct fd_range {
 	unsigned int from, to;
 };
-struct files_struct *dup_fd(struct files_struct *, struct fd_range *) __latent_entropy;
+struct files_struct *dup_fd(struct files_struct *oldf, int *errorp) __latent_entropy;
 void do_close_on_exec(struct files_struct *);
 int iterate_fd(struct files_struct *, unsigned,
 		int (*)(const void *, struct file *, unsigned),
