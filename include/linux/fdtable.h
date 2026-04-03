bf2191504d610855e4c5599295e8241bebd18e9d
gyabii <gyabii@proton.me>
2026-04-03 12:07:26 +0300

    fix

--- include/linux/fdtable.h
+++ include/linux/fdtable.h
@@ -127,7 +127,7 @@ extern int __close_fd(struct files_struct *files,
 extern int __close_range(struct files_struct *files, unsigned int fd,
 			 unsigned int max_fd);
 extern int __close_fd_get_file(unsigned int fd, struct file **res);
-extern int __close_range(unsigned int fd, unsigned int max_fd, unsigned int flags);
+extern int close_range(unsigned int fd, unsigned int max_fd, unsigned int flags);
 extern int close_fd_get_file(unsigned int fd, struct file **res);
 
 extern struct kmem_cache *files_cachep;
