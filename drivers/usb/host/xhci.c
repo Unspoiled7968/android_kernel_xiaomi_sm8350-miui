f3151dda97d5f30f4cc4a0db909f7fc7555690b5
kernel-upstream-test <gochmyradov0@gmail.com>
2026-07-27 00:52:57 +0200

    dwc3 ep0 test modes and a duplicated xhci helper
    - dwc3/ep0.c: same USB_TEST_* rename as gadget.c.
    - xhci: the merge left two definitions of xhci_handshake_check_state(), the
      CAF 5-argument one and 5.10's 6-argument exit_state form. Keep the latter,
      which is what xhci.h declares and xhci-ring.c calls.

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -83,27 +83,6 @@ int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, u64 timeout_us)
 	return ret;
 }
 
-int xhci_handshake_check_state(struct xhci_hcd *xhci,
-		void __iomem *ptr, u32 mask, u32 done, int usec)
-{
-	u32	result;
-
-	do {
-		result = readl_relaxed(ptr);
-		if (result == ~(u32)0)	/* card removed */
-			return -ENODEV;
-		/* host removed. Bail out */
-		if (xhci->xhc_state & XHCI_STATE_REMOVING)
-			return -ENODEV;
-		result &= mask;
-		if (result == done)
-			return 0;
-		udelay(1);
-		usec--;
-	} while (usec > 0);
-	return -ETIMEDOUT;
-}
-
 /*
  * xhci_handshake_check_state - same as xhci_handshake but takes an additional
  * exit_state parameter, and bails out with an error immediately when xhc_state
